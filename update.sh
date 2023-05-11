419c1e75e11aa84cdcb6e83e52628a8a49d356c2
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2023-05-10 20:29:52 -0400

    Added requirements install

--- update.sh
+++ update.sh
@@ -2,8 +2,15 @@
 
 REPO_URL="https://github.com/Atropa-Solanaceae/proXXy"
 
+# Install required packages
+echo "Installing required packages..."
+pip install -r requirements.txt
+
 # Check if Git is installed
 command -v git >/dev/null 2>&1 || { echo >&2 "Git is required but not installed. Aborting."; exit 1; }
+
+# Check if pip is installed
+command -v pip >/dev/null 2>&1 || { echo >&2 "pip is required but not installed. Aborting."; exit 1; }
 
 # Clone or update the repository
 if [ -d "proXXy" ]; then
@@ -16,6 +23,10 @@ else
     cd proXXy || exit
 fi
 
+# Install required packages
+echo "Installing required packages..."
+pip install -r requirements.txt
+
 # Copy the updated files to the current directory
 echo "Copying files..."
 cp -R ./* ..
@@ -23,5 +34,6 @@ cd .. || exit
 
 echo "Cleaning up..."
 rm -rf proXXy
+
 
 echo "Update completed."

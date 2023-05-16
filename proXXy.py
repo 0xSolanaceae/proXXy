32aafd41f94e6828c7a87eb1d74dc1c1a3d2e441
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2023-05-16 13:02:41 -0400

    Tabbed forward issues

--- proXXy.py
+++ proXXy.py
@@ -419,12 +419,12 @@ def scraping_handler(error_log, site, timeout):
     #print(f"|| {len(http_valid_proxies)} of {len(http_proxies)} ({http_percentage:.2f}%) HTTP proxies are currently active.")
     #print(f"|| {len(https_valid_proxies)} of {len(https_proxies)} ({https_percentage:.2f}%) HTTPS proxies are currently active.")
 
-    print()
-    with suppress(Exception):
-        print(f"|| {len(http_valid_proxies)} of {len(http_proxies)} ({http_percentage:.2f}%) HTTP proxies are currently active.")
-    with suppress(Exception):
-        print(f"|| {len(https_valid_proxies)} of {len(https_proxies)} ({https_percentage:.2f}%) HTTPS proxies are currently active.")
-    exit_con()
+        print()
+        with suppress(Exception):
+            print(f"|| {len(http_valid_proxies)} of {len(http_proxies)} ({http_percentage:.2f}%) HTTP proxies are currently active.")
+        with suppress(Exception):
+            print(f"|| {len(https_valid_proxies)} of {len(https_proxies)} ({https_percentage:.2f}%) HTTPS proxies are currently active.")
+        exit_con()
 
 def exit_con():
     text = "||     Thank you for using proXXy.     ||"

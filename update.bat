86e8da7762c3701db861699b4eb337201486bb8a
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2024-02-25 15:50:18 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,20 +1,17 @@
 @echo off
 set REPO_URL="https://github.com/Atropa-Solanaceae/proXXy"
-REM Check if Git is installed
 where git >nul 2>&1
 if %errorlevel% neq 0 (
     echo Git is required but not installed. Aborting.
     exit /b 1
 )
 
-REM Check if pip is installed
 where pip >nul 2>&1
 if %errorlevel% neq 0 (
     echo pip is required but not installed. Aborting.
     exit /b 1
 )
 
-REM Clone or update the repository
 if exist "proXXy" (
     echo Updating existing repository...
     cd proXXy || exit /b
@@ -27,18 +24,14 @@ if exist "proXXy" (
     cd proXXy || exit /b
 )
 
-REM Install required packages
 echo Installing required packages...
 pip install -r requirements.txt
 
-REM Copy the updated files to the current directory
 echo Copying files...
 xcopy /E /Y .\* ..
-REM Clean up
 echo Cleaning up...
 pushd ..
 rmdir /S /Q proXXy
 popd
-
 
 echo Update completed.

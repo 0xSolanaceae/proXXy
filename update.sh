8936daa8f399f06fcd91d364f9ca29dc9d1987dc
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2024-02-25 15:49:43 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,19 +3,16 @@
 REPO_URL="https://github.com/Atropa-Solanaceae/proXXy"
 REPO_DIR="proXXy"
 
-# Check if Git is installed
 if ! command -v git &>/dev/null; then
     echo >&2 "Git is required but not installed. Aborting."
     exit 1
 fi
 
-# Check if pip is installed
 if ! command -v pip &>/dev/null; then
     echo >&2 "pip is required but not installed. Aborting."
     exit 1
 fi
 
-# Clone or update the repository
 if [ -d "$REPO_DIR" ]; then
     echo "Updating existing repository..."
     cd "$REPO_DIR"
@@ -26,15 +23,12 @@ else
     cd "$REPO_DIR" || exit
 fi
 
-# Install required packages
 echo "Installing required packages..."
 pip install -r requirements.txt
 
-# Copy the updated files to the parent directory
 echo "Copying files..."
 cp -R ./* ..
 
-# Clean up
 echo "Cleaning up..."
 cd ..
 rm -rf "$REPO_DIR"

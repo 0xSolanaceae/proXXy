05b81f663e48a79f9388ab8cef81d07e75b4ad2f
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2024-02-28 15:53:52 -0500

    Update maintenance

--- update.sh
+++ update.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
 
+echo "[*] Updating proXXy..."
+
 REPO_URL="https://github.com/Atropa-Solanaceae/proXXy"
 REPO_DIR="proXXy"
+REQUIREMENTS_FILE="requirements.txt"
 
-if ! command -v git &>/dev/null; then
-    echo >&2 "Git is required but not installed. Aborting."
-    exit 1
-fi
+check_dependencies() {
+    command -v git &>/dev/null || { echo >&2 "[-] git is required but not installed. Aborting."; exit 1; }
+    command -v pip &>/dev/null || { echo >&2 "[-] pip is required but not installed. Aborting."; exit 1; }
+}
 
-if ! command -v pip &>/dev/null; then
-    echo >&2 "pip is required but not installed. Aborting."
-    exit 1
-fi
+update_repository() {
+    if [ -d "$REPO_DIR" ]; then
+        echo "[*] Updating existing repository..."
+        cd "$REPO_DIR" || exit
+        git pull origin
+    else
+        echo "[*] Cloning repository..."
+        git clone "$REPO_URL" "$REPO_DIR"
+        cd "$REPO_DIR" || exit
+    fi
+}
 
-if [ -d "$REPO_DIR" ]; then
-    echo "Updating existing repository..."
-    cd "$REPO_DIR"
-    git pull origin
-else
-    echo "Cloning repository..."
-    git clone "$REPO_URL" "$REPO_DIR"
-    cd "$REPO_DIR" || exit
-fi
+install_requirements() {
+    echo "[*] Installing required packages..."
+    pip install -r "$REQUIREMENTS_FILE"
+}
 
-echo "Installing required packages..."
-pip install -r requirements.txt
+copy_files() {
+    echo "[*] Copying files..."
+    cp -R ./* ..
+}
 
-echo "Copying files..."
-cp -R ./* ..
+clean_up() {
+    echo "[*] Cleaning up..."
+    cd ..
+    rm -rf "$REPO_DIR"
+}
 
-echo "Cleaning up..."
-cd ..
-rm -rf "$REPO_DIR"
+main() {
+    check_dependencies
+    update_repository
+    install_requirements
+    copy_files
+    clean_up
+    echo "[+] Update completed."
+}
 
-echo "Update completed."
+main

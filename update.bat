7fadd206233f810e9c92c977dcd136bdc8779daa
Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2024-02-28 15:48:38 -0500

    Update maintenance

--- update.bat
+++ update.bat
@@ -1,5 +1,16 @@
 @echo off
+
+echo Updating proXXy...
+
 set REPO_URL="https://github.com/Atropa-Solanaceae/proXXy"
+echo Checking for required software...
+
+where python >nul 2>&1
+if %errorlevel% neq 0 ( 
+    echo Python is required but not installed. Aborting.
+    exit /b 1
+)
+
 where git >nul 2>&1
 if %errorlevel% neq 0 (
     echo Git is required but not installed. Aborting.
@@ -16,9 +27,7 @@ if exist "proXXy" (
     echo Updating existing repository...
     cd proXXy || exit /b
     git pull origin
-
 ) else (
-
     echo Cloning repository...
     git clone %REPO_URL% proXXy
     cd proXXy || exit /b
@@ -29,9 +38,10 @@ pip install -r requirements.txt
 
 echo Copying files...
 xcopy /E /Y .\* ..
+
 echo Cleaning up...
 pushd ..
 rmdir /S /Q proXXy
 popd
 
-echo Update completed.
+echo Update completed, enjoy your new proXXy!

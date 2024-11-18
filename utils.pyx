45735ddc3955b77735f22d8c1502078af4bd9cab
Atropa-Solanaceae <89823371+Atropa-Solanaceae@users.noreply.github.com>
2024-11-18 13:29:10 -0500

    add in static type def

--- utils.pyx
+++ utils.pyx
@@ -1,6 +1,7 @@
 # cython: language_level=3
 # -*- coding: utf-8 -*-
 
+import contextlib
 import os
 import json
 import time
@@ -9,152 +10,37 @@ import requests
 from tqdm import tqdm
 from yaspin import yaspin
 from concurrent.futures import ThreadPoolExecutor
+from cython.parallel import prange
 
-cpdef dict cproxy_sources():
-    return {
-        "HTTP": [
-            "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/HTTP.txt",
-            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
-            "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
-            "https://api.proxyscrape.com/?request=getproxies&proxytype=https&timeout=10000&country=all&ssl=all&anonymity=all",
-            "https://api.openproxylist.xyz/http.txt",
-            "https://alexa.lr2b.com/proxylist.txt",
-            "https://multiproxy.org/txt_all/proxy.txt",
-            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
-            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_anonymous/http.txt",
-            "https://raw.githubusercontent.com/shiftytr/proxy-list/master/proxy.txt",
-            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
-            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
-            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
-            "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
-            "https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt",
-            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
-            "https://raw.githubusercontent.com/opsxcq/proxy-list/master/list.txt",
-            "https://raw.githubusercontent.com/proxy4parsing/proxy-list/main/http.txt",
-            "https://rootjazz.com/proxies/proxies.txt",
-            "https://spys.me/proxy.txt",
-            "https://proxyspace.pro/http.txt",
-            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/http.txt",
-            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies_anonymous/http.txt",
-            "https://raw.githubusercontent.com/zevtyardt/proxy-list/main/http.txt",
-            "https://sunny9577.github.io/proxy-scraper/proxies.txt",
-            "https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/http.txt",
-            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/http.txt",
-            "https://raw.githubusercontent.com/prxchk/proxy-list/main/http.txt",
-            "https://raw.githubusercontent.com/ALIILAPRO/Proxy/main/http.txt",
-            "https://raw.githubusercontent.com/zloi-user/hideip.me/main/http.txt",
-            "https://raw.githubusercontent.com/saisuiu/Lionkings-Http-Proxys-Proxies/main/cnfree.txt",
-            "https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/main/proxy_files/http_proxies.txt",
-            "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/http/http.txt",
-            "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/http.txt",
-            "https://raw.githubusercontent.com/elliottophellia/yakumo/master/results/http/global/http_checked.txt",
-        ],
-        "SOCKS4": [
-            "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4",
-            "https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&country=all",
-            "https://api.openproxylist.xyz/socks4.txt",
-            "https://proxyspace.pro/socks4.txt",
-            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt",
-            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_anonymous/socks4.txt",
-            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt",
-            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
-            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt",
-            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
-            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
-            "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/SOCKS4.txt",
-            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks4.txt",
-            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies_anonymous/socks4.txt",
-            "https://raw.githubusercontent.com/zevtyardt/proxy-list/main/socks4.txt",
-            "https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks4.txt",
-            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/socks4.txt",
-            "https://raw.githubusercontent.com/prxchk/proxy-list/main/socks4.txt",
-            "https://raw.githubusercontent.com/ALIILAPRO/Proxy/main/socks4.txt",
-            "https://raw.githubusercontent.com/zloi-user/hideip.me/main/socks4.txt",
-            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
-            "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/SOCKS4.txt",
-            "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/socks4.txt",
-            "https://raw.githubusercontent.com/elliottophellia/yakumo/master/results/socks4/global/socks4_checked.txt",
-        ],
-        "SOCKS5": [
-            "https://raw.githubusercontent.com/B4RC0DE-TM/proxy-list/main/SOCKS5.txt",
-            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt",
-            "https://api.openproxylist.xyz/socks5.txt",
-            "https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5",
-            "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5",
-            "https://proxyspace.pro/socks5.txt",
-            "https://raw.githubusercontent.com/manuGMG/proxy-365/main/SOCKS5.txt",
-            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt",
-            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies_anonymous/socks5.txt",
-            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
-            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
-            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
-            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
-            "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
-            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks5.txt",
-            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies_anonymous/socks5.txt",
-            "https://raw.githubusercontent.com/zevtyardt/proxy-list/main/socks5.txt",
-            "https://raw.githubusercontent.com/MuRongPIG/Proxy-Master/main/socks5.txt",
-            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/socks5.txt",
-            "https://raw.githubusercontent.com/prxchk/proxy-list/main/socks5.txt",
-            "https://raw.githubusercontent.com/ALIILAPRO/Proxy/main/socks5.txt",
-            "https://spys.me/socks.txt",
-            "https://raw.githubusercontent.com/zloi-user/hideip.me/main/socks5.txt",
-            "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/socks5.txt",
-            "https://raw.githubusercontent.com/elliottophellia/yakumo/master/results/socks5/global/socks5_checked.txt",
-        ],
-        "HTTPS": [
-            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
-            "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/https.txt",
-            "https://proxyspace.pro/https.txt",
-            "https://raw.githubusercontent.com/zloi-user/hideip.me/main/https.txt",
-            "https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/main/proxy_files/https_proxies.txt",
-            "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt",
-            "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/https.txt"
-        ]
-    }
-
-import os
-import json
-import time
-import logging
-import requests
-from tqdm import tqdm
-from yaspin import yaspin
-from concurrent.futures import ThreadPoolExecutor
-
-def load_proxy_sources(file_path):
+def load_proxy_sources(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         return json.load(file)
 
-def proxy_sources():
+def proxy_sources() -> dict:
     return load_proxy_sources('proxy_sources.json')
 
-def http_check(PROXY_LIST_FILE): 
+def http_check(PROXY_LIST_FILE: str): 
     # combine two methods into one, so that when it's called it'll differentiate based upon what type of proxy parameter is fed into it
-    TEST_URL = "http://httpbin.org/ip" #https://httpbin.org/anything #https://api.myip.com/ #FIXME add in time to reach judge sites in milliseconds
-    TIMEOUT = 5
-    logging.basicConfig(filename='error.log', level=logging.ERROR)
+    cdef str TEST_URL = "http://httpbin.org/ip" # https://httpbin.org/anything # https://api.myip.com/ #FIXME add in time to reach judge sites in milliseconds
+    cdef int TIMEOUT = 5
+    logging.basicConfig(filename='output/error.log', level=logging.ERROR)
 
-    def test_proxy(proxy):
-        try:
+    def test_proxy(proxy: str) -> str:
+        with contextlib.suppress(Exception):
             response = requests.get(TEST_URL, proxies={'http': proxy}, timeout=TIMEOUT)
             if 100 <= response.status_code < 400:
                 return proxy
-        except Exception:
-            pass
         return None
 
     def main():
-        start_time = time.time()
-        http_proxies = []
+        cdef float start_time = time.time()
+        cdef list http_proxies = []
 
         with open(PROXY_LIST_FILE, 'r') as f:
             http_proxies = [line.strip() for line in f.readlines()]
-        
-        cpu_count = os.cpu_count()
-        max_threads = cpu_count * 420
-        if not max_threads:
-            max_threads = 1000
+
+        cdef int cpu_count = os.cpu_count()
+        cdef int max_threads = cpu_count * 420 or 1000
 
         print(f"[*] Utilizing {max_threads:,} threads, calculated from your device's {cpu_count} CPU cores.")
 
@@ -172,47 +58,44 @@ def http_check(PROXY_LIST_FILE):
                     results.append(result)
 
         http_valid_proxies = [proxy for proxy in results if proxy is not None]
-        
-        end_time = time.time()
-        total_time = end_time - start_time
-        total_minutes = total_time // 60
-        remaining_seconds = total_time % 60
+
+        cdef float end_time = time.time()
+        cdef float total_time = end_time - start_time
+        cdef int total_minutes = total_time // 60
+        cdef float remaining_seconds = total_time % 60
         print("[*] Total time for Execution:", f"{int(total_minutes)} minute(s) and {round(remaining_seconds, 2)} seconds")
-        
+
         print(f"[*] Valid HTTP Proxies: {len(http_valid_proxies):,}\n")
 
         with open(PROXY_LIST_FILE, 'w') as f:
             for proxy in http_valid_proxies:
                 f.write(proxy + '\n')
+
     main()
     
-def https_check(PROXY_LIST_FILE):
-    #FIXME tell the user the anonymity level of the proxy
-    #FIXME add in socks4/5 verification
-    TEST_URL = "https://api.myip.com:443/"
-    TIMEOUT = 5
+def https_check(PROXY_LIST_FILE: str):
+    # FIXME tell the user the anonymity level of the proxy
+    # FIXME add in socks4/5 verification
+    cdef str TEST_URL = "https://api.myip.com:443/"
+    cdef int TIMEOUT = 5
     logging.basicConfig(filename='error.log', level=logging.ERROR)
 
-    def test_proxy(proxy):
-        try:
+    def test_proxy(proxy: str) -> str:
+        with contextlib.suppress(Exception):
             response = requests.get(TEST_URL, proxies={'https': proxy}, timeout=TIMEOUT)
             if 100 <= response.status_code < 400:
                 return proxy
-        except Exception:
-            pass
         return None
 
     def main():
-        start_time = time.time()
-        https_proxies = []
+        cdef float start_time = time.time()
+        cdef list https_proxies = []
 
         with open(PROXY_LIST_FILE, 'r') as f:
             https_proxies = [line.strip() for line in f.readlines()]
-        
-        cpu_count = os.cpu_count()
-        max_threads = cpu_count * 250
-        if not max_threads:
-            max_threads = 1000
+
+        cdef int cpu_count = os.cpu_count()
+        cdef int max_threads = cpu_count * 250 or 1000
 
         print(f"[*] Utilizing {max_threads:,} threads, calculated from your device's {cpu_count} CPU cores.")
 
@@ -230,16 +113,17 @@ def https_check(PROXY_LIST_FILE):
                     results.append(result)
 
         https_valid_proxies = [proxy for proxy in results if proxy is not None]
-        
-        end_time = time.time()
-        total_time = end_time - start_time
-        total_minutes = total_time // 60
-        remaining_seconds = total_time % 60
+
+        cdef float end_time = time.time()
+        cdef float total_time = end_time - start_time
+        cdef int total_minutes = total_time // 60
+        cdef float remaining_seconds = total_time % 60
         print("[*] Total time for Execution:", f"{int(total_minutes)} minute(s) and {round(remaining_seconds, 2)} seconds")
-        
+
         print(f"[*] Valid HTTPS Proxies: {len(https_valid_proxies):,}")
 
         with open(PROXY_LIST_FILE, 'w') as f:
             for proxy in https_valid_proxies:
                 f.write(proxy + '\n')
+
     main()
